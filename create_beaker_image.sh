08def6ae9eccb8a7be21c3cd9b13bbf917d8f2ef
Kyle Richardson <kyler@allenai.org>
2020-12-13 16:54:18 -0800

    added the remaining items

---
+++ create_beaker_image.sh
@@ -0,0 +1,11 @@
+CURRENT=1
+IMAGE_NAME=transformer_tools_v1
+DOCKERFILE_NAME=Dockerfile
+
+GIT_HASH=`git log --format="%h" -n 1`
+IMAGE=$IMAGE_NAME_$USER-$GIT_HASH
+IM_NAME=${IMAGE}_1
+
+echo "Building $IMAGE"
+docker build -f $DOCKERFILE_NAME -t $IMAGE .
+beaker image create --name=${IM_NAME}_${CURRENT} --desc="TransformerToolsV${CURRENT}" $IMAGE
---
+++ run.sh
@@ -0,0 +1,1 @@
+python -m transformer_tools "$@"
