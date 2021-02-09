549128307bc6c76938e8d1e7c41c32ceb0935d84
Ronen <ronen.tamari1@mail.huji.ac.il>
2021-02-08 21:43:06 -0800

    add option to bypass preproc and just push folder to wandb

--- scripts/prepare_data.py
+++ scripts/prepare_data.py
@@ -2,14 +2,15 @@
 Convert data dir of files in bAbI format to T5 format, and add as artifact to folder in wandb
    
 Usage:
-    prepare_data.py [--source_dir=<source_dir> --save_name=<save_name> --wandb]
+    prepare_data.py [--source_dir=<source_dir> --save_name=<save_name> --wandb --just_push_wandb]
 
                          
 Options:
    -h --help     Show this screen.
    --source_dir=<source_dir>    Directory containing data in bAbI format. [default: .]
    --save_name=<save_name>    Directory to save data to (in '_data' dir).  [default: default]
-   --wandb   If True, save also as artifact on wandb
+   --just_push_wandb   If True, skip data-preprocessing and just push `save_name` folder to wandb. [default: False]
+   --wandb   If True, save also as artifact on wandb.
 
 """
 
@@ -37,19 +38,23 @@ if __name__ == "__main__":
     
     source_dir = Path(args["--source_dir"])
     save_name = DATA_DIR / args["--save_name"]
-    add_to_wandb = args["--wandb"]
+    just_wandb = args["--just_push_wandb"]
+    add_to_wandb = args["--wandb"] or just_wandb
     
     # create save dir
     save_name.mkdir(parents=True, exist_ok=True)
     
-    logger.info(f"Preparing data at {str(save_name)}...")
     
-    cmd = [PY_BIN, str(CONVERT_SCRIPT), "--data_loc", str(source_dir), "--odir", str(save_name)]
-    print(' '.join(cmd))
-    try:
-        res = subprocess.run(cmd, cwd=str(ROOT), capture_output=True)
-    except subprocess.CalledProcessError as e:
-        print(e.output)
+    # check if we want to skip preprocessing and just push some folder to wandb
+    if not just_wandb:
+        logger.info(f"Preparing data at {str(save_name)}...")
+        
+        cmd = [PY_BIN, str(CONVERT_SCRIPT), "--data_loc", str(source_dir), "--odir", str(save_name)]
+        print(' '.join(cmd))
+        try:
+            res = subprocess.run(cmd, cwd=str(ROOT), capture_output=True)
+        except subprocess.CalledProcessError as e:
+            print(e.output)
         
     if add_to_wandb:
         wand_save_path = str(save_name.relative_to(ROOT))
