07dc9b87f0afb757ff2e6cf332f6554764f1fecb
Kyle Richardson <kyler@allenai.org>
2021-02-11 17:49:05 -0800

    updated demo to have roberta combined model

--- polarity_demo.sh
+++ polarity_demo.sh
@@ -1,4 +1,4 @@
 
 streamlit run bin/polarity_demo.py -- \
-          --wandb_model "polarity_projection/polarity_model_runs/distilbert_combined_model:v0" \
+          --wandb_model "polarity_projection/polarity_model_runs/roberta_combined_model:v0" \
           --wandb_entity "polarity_projection"
