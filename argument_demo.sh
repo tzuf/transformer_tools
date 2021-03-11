b5878b54a984b3e3fdeab39565224948d050f477
Kyle Richardson <kyler@allenai.org>
2021-03-10 23:02:29 -0800

    changed the port in the argument_demo

--- argument_demo.sh
+++ argument_demo.sh
@@ -3,6 +3,6 @@ if [ ! $WANDB_API_KEY ]; then
     exit
 fi
 
-streamlit run bin/argument_demo.py -- \
+streamlit run bin/argument_demo.py --server.port 9050 -- \
           --wandb_model "aaac/aaac_model_runs/multi_angle_exp_model:v0" \
           --wandb_entity aaac
